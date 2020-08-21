06b56d60bc97b287479f8161685c73f162cfb50c
Ethan Chiu <ethanchewy@gmail.com>
2020-08-21 14:18:59 -0400

    Update reverseInParentheses.py

--- codesignal/reverseInParentheses.py
+++ codesignal/reverseInParentheses.py
@@ -1,8 +1,8 @@
-```
+"""
 Write a function that reverses characters in (possibly nested) parentheses in the input string.
 
 Input strings will always be well-formed with matching ()s.
-```
+"""
 
 def reverseInParentheses(inputString):
     final_string = ""
